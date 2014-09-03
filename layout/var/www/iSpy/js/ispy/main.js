f5dc665f51a4b9d4684a198e66fad2ffe966951f
moloch <moloch38@gmail.com>
2014-09-02 19:19:10 -0700

    Minor tweaks to ui layout, and basic Backbone models/renderer

--- layout/var/www/iSpy/js/ispy/main.js
+++ layout/var/www/iSpy/js/ispy/main.js
@@ -1,76 +1,121 @@
-var jsonrpc = undefined;
-var msgSendState = false;
+/*
+ * iSpy - JavaScript application
+ */
+(function() {
 
-var enableMsgSend = {
-    "messageType":"setMsgSendLoggingState",
-    "messageData": {
-        "state":"true"
-    },
-    "responseId":"response31337"
-}
+    /* iSpy Namespace */
+    window.iSpy = {
+        Models: {},
+        Collections: {},
+        Views: {},
+    };
 
-var disableMsgSend = {
-    "messageType":"setMsgSendLoggingState",
-    "messageData": {
-        "state":"false"
-    },
-    "responseId":"response31337"
-}
+    /*
+     * This is a template helper function to automatically pull the template
+     * markup from the main DOM using jquery.
+     */
+    window.template = function(id) {
+        return _.template( $('#' + id).html() );
+    };
 
-function escapeHtml(value) {
-    return $('<div/>').text(value).html();
-}
 
-function add_objc_msg(msg) {
-    argmsg = "";
-    if ('args' in msg) {
-        for (index = 0; index < msg.args.length; ++index) {
-            argmsg = msg.args[index]['type'] + ": " + msg.args[index]['name'] + "<" + msg.args[index]['addr'] + ">";
-            msgsend += argmsg + ", ";
-        }
-    }
-    methodType = msg['isInstanceMethod'] ? "- ":"+ ";
-    $('#objc-msg-send-table').prepend(
-        $("<tr/>").append(
-            $("<td/>").text($('#objc-msg-send-table tr').length + 1)
-        ).append(
-            $("<td/>").text(msg['class'])
-        ).append(
-            $("<td/>").text(methodType + msg['method'])
-        ).append(
-            $("<td/>").text(argmsg)
-        ).append(
-            $("<td/>").text("(" + msg['returnValue']['type'] + ")" + msg['returnValue']['value'])
-        )
-    );
-}
+    iSpy.Models.ObjcClass = Backbone.Model.extend({
 
-$(document).ready(function() {
+    });
 
-    $("#btnMsgSendState").click(function() {
-        if (msgSendState) {
-            jsonrpc.send(JSON.stringify(disableMsgSend));
-            msgSendState = false;
-        } else {
-            jsonrpc.send(JSON.stringify(enableMsgSend));
-            msgSendState = true;
+    iSpy.Views.ObjcClass = Backbone.View.extend({
+
+        tagName: 'li',
+
+        template: template('ObjcClassTemplate'),
+
+        events: {
+        },
+
+        render: function() {
+            var template = this.template(this.model.toJSON());
+            this.$el.html(template);
+            return this;
         }
 
     });
 
+    iSpy.Views.ObjcClasses = Backbone.View.extend({
+
+        tagName: 'ul',
+
+        render: function() {
+            this.collection.each(this.addOne, this);
+            return this;
+        },
+
+        /* Renders a single class view and appends it to the collection view */
+        addOne: function(klass) {
+            var klassView = new iSpy.Views.ObjcClass({ model: klass });
+            this.$el.append(klassView.render().el);
+        }
+
+    });
+
+    iSpy.Collections.ObjcClasses = Backbone.Collection.extend({
+        model: iSpy.Models.ObjcClass,
+    });
+
+
+
+
+
+var klass = new iSpy.Models.ObjcClass({
+    'title': 'NSString',
+    size: 4
+});
+
+var klasses = new iSpy.Collections.ObjcClasses(
+[
+    {
+        'title': 'NSString',
+        size: 4
+    },
+    {
+        'title': 'NSString',
+        size: 6
+    },
+    {
+        'title': 'NSArray',
+        size: 12
+    },
+]);
+
+var klassesView = new iSpy.Views.ObjcClasses({collection: klasses});
+document.body.append(klassesView.render().el);
+
+
+})();
+
+
+$(document).ready(function() {
+
+    /* Menu Setup */
+    $.slidebars();
+    jQuery('ul.nav li.dropdown').hover(function() {
+        jQuery(this).find('.dropdown-menu').stop(true, true).delay(200).show();
+    }, function() {
+        jQuery(this).find('.dropdown-menu').stop(true, true).delay(200).hide();
+    });
+
+    /* WebSocket Setup */
     var jsonrpc_url = "ws://" + window.location.host + "/jsonrpc";
     console.log("[*] Connecting to json-rpc server -> " + jsonrpc_url);
-
     jsonrpc = new WebSocket(jsonrpc_url);
 
+    /* WebSocket Opened */
     jsonrpc.onopen = function() {
         console.log("[*] Successfully connected to remote rpc server!");
         $("#activity-monitor").removeClass("fa-eye-slash");
         $("#activity-monitor").addClass("fa-refresh fa-spin");
-        console.log("[*] Sending the enableMsgSend message ...");
-
     }
 
+    /* WebSocket Message */
     jsonrpc.onmessage = function(emit) {
         console.log(emit);
         var msg = jQuery.parseJSON(emit.data);
@@ -79,6 +124,7 @@ $(document).ready(function() {
         }
     }
 
+    /* WebSocket Closed */
     jsonrpc.onclose = function() {
         console.log("[!] Connection to server lost!");
         $("#activity-monitor").removeClass("fa-refresh fa-spin");

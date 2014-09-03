a0e7fc611fae27663a96e03bcd803a8dd0476a5c
moloch <moloch38@gmail.com>
2014-09-03 14:43:12 -0700

    Removed old OpenSSL directory

--- layout/var/www/iSpy/js/ispy/models.js
+++ layout/var/www/iSpy/js/ispy/models.js
@@ -1,6 +1,34 @@
 /*
- * Objective-C Class
+ * Application
+ */
+iSpy.Models.App = Backbone.Model.extend({
+    defaults: {
+        classdump: [],
+        
+    },
+
+
+});
+
+/*
+ * Objective-C
  */
 iSpy.Models.ObjcClass = Backbone.Model.extend({
+    defaults: {
+        name: null,
+        methods: [],
+        properties: [],
+        ivars: [],
+    },
+
+
+});
+
+iSpy.Models.ObjcMethod = Backbone.Model.extend({
+    defaults: {
+        instanceMethod: false,
+        parameters: [],
+    },
+
 
 });

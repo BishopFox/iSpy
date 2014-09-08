5c9c94950c484a855409198894b48587293ace3f
moloch <moloch38@gmail.com>
2014-09-07 17:16:58 -0700

    Moved backbone sync code into namespace.js

--- layout/var/www/iSpy/js/ispy/namespace.js
+++ layout/var/www/iSpy/js/ispy/namespace.js
@@ -42,13 +42,32 @@ window.iSpy = {
     }
 
     iSpy.SyncSocket.onclose = function() {
-        console.log("[!] Connection to server lost!");
+        console.log("[!] Connection to server lost");
         $("#activity-monitor").removeClass("fa-refresh fa-spin");
         $("#activity-monitor").addClass("fa-eye-slash");
         iSpy.Events.trigger("ispy:connection-lost");
     }
 
 })(jQuery);
+
+
+/*
+ *   We create our custom CRUD here. This function is only responsible for sending data
+ *   to the server, iSpy.SyncSocket.onmessage is responsible for handling responses from
+ *   the server, which may or may not have been triggered by a request sent from here.
+ */
+Backbone.sync = function(method, model, options) {
+
+    if (method === "read") {
+        for (var index = 0; index < model.rpcRead.length; ++index) {
+            var readMessage = JSON.stringify(model.rpcRead[index]);
+            console.log("[Backone.Sync:Read] -> " + readMessage);
+            iSpy.SyncSocket.send(readMessage);
+        }
+    }
+
+};
+
 
 /* Little helper template function */
 window.mktemplate = function(id) {
